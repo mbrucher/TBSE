2c569da87771acb1deed41d837a004af317d6700
Matthieu Brucher <matthieu.brucher@gmail.com>
2012-04-02 22:33:27 +0200

    Default implementation of the registry OK

--- Core/registry.cpp
+++ Core/registry.cpp
@@ -11,4 +11,28 @@ namespace TGSE
     static Registry registry;
     return registry;
   }
+
+  Registry::Registry()
+  {
+  }
+
+  void Registry::set_function(const std::string& name, RegistryFunction function)
+  {
+    registry[name] = function;
+  }
+
+  Registry::RegistryFunction Registry::get_function(const std::string& name) const
+  {
+    RegistryInnerType::const_iterator it = registry.find(name);
+    if (it == registry.end())
+    {
+      throw std::runtime_error("No such loaded plugins");
+    }
+    return it->second;
+  }
+
+  RegistryLoader::RegistryLoader(const std::string& name, Registry::RegistryFunction function)
+  {
+    Registry::get().set_function(name, function);
+  }
 }
--- Core/registry.h
+++ Core/registry.h
@@ -5,15 +5,36 @@
 #ifndef REGISTRY_H
 #define REGISTRY_H
 
+#include <map>
+#include <string>
+
 #include "TGSE_Core_config.h"
 
 namespace TGSE
 {
   class Registry
   {
+    Registry();
+  public:
+    typedef void (*RegistryFunction)();
+  private:
+    typedef std::map<std::string, RegistryFunction> RegistryInnerType;
+    RegistryInnerType registry;
   public:
     TGSE_Core_EXPORT static Registry& get();
+
+    TGSE_Core_EXPORT void set_function(const std::string& name, RegistryFunction function);
+    TGSE_Core_EXPORT RegistryFunction get_function(const std::string& name) const;
+  };
+
+  class RegistryLoader
+  {
+  public:
+    TGSE_Core_EXPORT RegistryLoader(const std::string& name, Registry::RegistryFunction function);
   };
 }
+
+#define TGSEEXPORTPLUGIN(name, function) \
+  TGSE::RegistryLoader registry_loader##name( #name, function);
 
 #endif
