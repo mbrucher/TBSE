52d7778b2320605a5a172450318723302f856330
Matthieu Brucher <matthieu.brucher@gmail.com>
2012-04-14 23:43:08 +0200

    Added an hexagon item to subclass pixmap item that will always have an hexagonal shape

---
+++ GUI/2DGUI/hexagon_item.cpp
@@ -0,0 +1,12 @@
+/**
+ * \file hexagon_item.cpp
+ */
+
+#include "hexagon_item.h"
+
+namespace TBSE
+{
+  namespace Qt
+  {
+  }
+}
---
+++ GUI/2DGUI/hexagon_item.h
@@ -0,0 +1,20 @@
+/**
+ * \file hexagon_item.h
+ */
+
+#ifndef HEXAGONITEM_H
+#define HEXAGONITEM_H
+
+#include <QGraphicsPixmapItem>
+
+namespace TBSE
+{
+  namespace Qt
+  {
+    class HexagonItem: public QGraphicsPixmapItem
+    {
+    };
+  }
+}
+
+#endif
