5f49b6ab265230cb4098f95f579bb6073fa2ef6c
Matthieu Brucher <matthieu.brucher@gmail.com>
2013-01-21 20:01:58 +0100

    Added verbosity
    Fixed loaded the plugins on OS X

--- env.sh
+++ env.sh
@@ -2,4 +2,4 @@
 export PATH=${HOME}/src/TBSE/bin:$PATH
 export LD_LIBRARY_PATH=${HOME}/src/TBSE/lib:$LD_LIBRARY_PATH
 export DYLD_LIBRARY_PATH=${HOME}/src/TBSE/lib:$DYLD_LIBRARY_PATH
-export TBSEPLUGINPATH=${HOME}/src/TBSE/lib/plugins
+export TBSEPLUGINPATH=${HOME}/src/TBSE/lib/plugins/:
