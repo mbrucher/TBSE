3328e76ff17b3413e93ad7356a949d991e532dd3
Matthieu Brucher <matthieu.brucher@gmail.com>
2013-03-17 19:31:32 +0000

    Added a mind map for explaining what needs to be done

---
+++ Docs/TBSE.mm
@@ -0,0 +1,92 @@
+<map version="freeplane 1.2.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="TBSE" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1363548495916"><hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="8"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<node TEXT="Standards" POSITION="right" ID="ID_777310371" CREATED="1363548219174" MODIFIED="1363548223268">
+<edge COLOR="#ff0000"/>
+<node TEXT="Naming convention" ID="ID_565661685" CREATED="1363548277154" MODIFIED="1363548284054">
+<node TEXT="CamelCase for classes and namespace" ID="ID_1375968716" CREATED="1363548285137" MODIFIED="1363548302901"/>
+<node TEXT="function_names" ID="ID_999740542" CREATED="1363548303338" MODIFIED="1363548308661"/>
+<node TEXT="variable_names" ID="ID_1079992712" CREATED="1363548309612" MODIFIED="1363548324179"/>
+</node>
+<node TEXT="Const whenever possible" ID="ID_353861715" CREATED="1363548329270" MODIFIED="1363548335411"/>
+</node>
+<node TEXT="Plugins" POSITION="right" ID="ID_1819805371" CREATED="1363548224706" MODIFIED="1363548345992">
+<edge COLOR="#0000ff"/>
+</node>
+<node TEXT="GUI" POSITION="left" ID="ID_106080857" CREATED="1363548348140" MODIFIED="1363548350781">
+<edge COLOR="#00ff00"/>
+<node TEXT="2D" ID="ID_778734623" CREATED="1363548351998" MODIFIED="1363548354082">
+<node TEXT="Qt" ID="ID_408738446" CREATED="1363548354827" MODIFIED="1363548356658"/>
+</node>
+</node>
+<node TEXT="AI" POSITION="left" ID="ID_372249438" CREATED="1363548359822" MODIFIED="1363548363005">
+<edge COLOR="#ff00ff"/>
+</node>
+<node TEXT="Concepts" POSITION="right" ID="ID_1329675284" CREATED="1363548474677" MODIFIED="1363548477695">
+<edge COLOR="#00ffff"/>
+<node TEXT="Hexagonal maps" ID="ID_516828778" CREATED="1363548479095" MODIFIED="1363548485112"/>
+<node TEXT="Units" ID="ID_1095827753" CREATED="1363548546869" MODIFIED="1363548556824"/>
+<node TEXT="Maps" ID="ID_1588400702" CREATED="1363548563563" MODIFIED="1363548565172"/>
+<node TEXT="Campaigns" ID="ID_124942231" CREATED="1363548565893" MODIFIED="1363548568773"/>
+</node>
+<node TEXT="Tools" POSITION="left" ID="ID_969653733" CREATED="1363548610348" MODIFIED="1363548611467">
+<edge COLOR="#ffff00"/>
+<node TEXT="Map editor" ID="ID_551518382" CREATED="1363548617418" MODIFIED="1363548620936"/>
+<node TEXT="Unit editor" ID="ID_1375566481" CREATED="1363548622406" MODIFIED="1363548626683"/>
+</node>
+<node TEXT="Scripting" POSITION="right" ID="ID_1426975825" CREATED="1363548631796" MODIFIED="1363548637168">
+<edge COLOR="#00007c"/>
+<node TEXT="Python" ID="ID_182732977" CREATED="1363548638448" MODIFIED="1363548640238"/>
+</node>
+</node>
+</map>
