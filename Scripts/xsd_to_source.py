9a9de5a84f2f59e49410fb2428896613709d3275
Matthieu Brucher <matthieu.brucher@gmail.com>
2013-03-26 11:34:39 +0000

    Using ElementTree instead of DOM

--- Scripts/xsd_to_source.py
+++ Scripts/xsd_to_source.py
@@ -1,19 +1,42 @@
 #!/usr/bin/env python
 
+class XSDContent(object):
+  pass
+
 def parse_xsd(filename):
-  from xml.dom.minidom import parse
+  from xml.etree.ElementTree import parse
   f = open(filename)
   xmlstructure = parse(f)
-  for child in xmlstructure.getElementsByTagNameNS("xs:schema", ""):
-    print child
-  structure = []
+  structure = XSDContent()
+  structure.filename = filename.replace('Schemas', 'Generated').replace('.xsd', '')
+  structure.includes = []
+  structure.objects = []
+
+  for child in xmlstructure.getroot():
+    kind = child.tag.split("}")[1]
+    print kind
+    if kind == "include":
+      pass
+    if kind == "element":
+      pass
+
   return structure
 
+def generate_folders(filename):
+  import os
+  dirname = os.path.dirname(filename)
+  if not os.path.exists(dirname):
+    os.makedirs(dirname)
+  
 def generate_header(structure):
-  pass
+  filename = structure.filename + '.h'
+  generate_folders(filename)
+  f = open(filename, "w")
 
 def generate_source(structure):
-  pass
+  filename = structure.filename + '.cpp'
+  generate_folders(filename)
+  f = open(filename, "w")
 
 if __name__ == "__main__":
   import sys
