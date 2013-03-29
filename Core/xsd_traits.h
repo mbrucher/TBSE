9febb09b24a9f5b31a45e05ddcc33e019d0ebabf
Matthieu Brucher <matthieu.brucher@gmail.com>
2013-03-29 16:30:18 +0000

    Oops…

--- Core/xsd_traits.h
+++ Core/xsd_traits.h
@@ -13,11 +13,11 @@ class QDomElement;
 template<typename DataType>
 struct XSDTraits
 {
-  void serialize(const DataType& object, QDomElement* fragment)
+  static void serialize(const DataType& object, QDomElement* fragment)
   {
     object.serialize(fragment);
   }
-  void unserialize(DataType& object, const QDomElement* fragment)
+  static void unserialize(DataType& object, const QDomElement* fragment)
   {
     object.unserialize(fragment);
   }
@@ -26,15 +26,15 @@ struct XSDTraits
 template<>
 struct XSDTraits<int>
 {
-  void serialize(const int& object, QDomElement* fragment);
-  void unserialize(int& object, const QDomElement* fragment);
+  static void serialize(const int& object, QDomElement* fragment);
+  static void unserialize(int& object, const QDomElement* fragment);
 };
 
 template<>
 struct XSDTraits<std::string>
 {
-  void serialize(const std::string& object, QDomElement* fragment);
-  void unserialize(std::string& object, const QDomElement* fragment);
+  static void serialize(const std::string& object, QDomElement* fragment);
+  static void unserialize(std::string& object, const QDomElement* fragment);
 };
 
 #endif /* CORE_XSD_TRAITS_H */
